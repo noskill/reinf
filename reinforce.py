27ff772024baa9e384de8728ea5b763beb325739
Anatoly Belikov <abelikov@singularitynet.io>
2025-01-24 16:51:26 +0300

    REINFORCE implementation

---
+++ reinforce.py
@@ -0,0 +1,55 @@
+import numpy
+import sys
+import gymnasium as gym
+from gymnasium.vector import SyncVectorEnv
+from torch import nn
+from agent import *
+
+
+class NA1(nn.Module):
+    def __init__(self, n_obs, n_action):
+        super().__init__()
+        self.layer1 = nn.Linear(n_obs, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_action)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x.to(self.layer1.weight)))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+def init_normal(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, mean=0, std=0.4)
+
+
+def make_env():
+    return gym.make('Pendulum-v1')
+
+# Create vectorized environment
+env = SyncVectorEnv([make_env for _ in range(16)])  # 8 environments
+num_batch = 10
+n_episodes = 10000
+dist_params = 2
+num_envs=16
+discount = 0.9
+na = NA1(3, 1 * dist_params)
+na.to(torch.device('cuda'))
+agent = Reinforce(na, sample_action_beta, num_envs=num_envs, discount=discount)
+agent.policy.apply(init_normal)
+
+for i in range(n_episodes):
+    obs, info = env.reset()
+    done = numpy.zeros(num_envs, dtype=bool)
+    agent.episode_start()
+    while not done.all():
+        action = agent.get_action(obs, done)
+        next_obs, reward, terminated, truncated, info = env.step(action)
+        done = terminated | truncated
+
+        agent.update(obs, action, reward, done, next_obs)
+        obs = next_obs
+    if i % 10 == 0:
+        print('iteration ' + str(i))
+    sys.stdout.flush()
